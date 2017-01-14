14df6da313e68417bd0e573ffd3e385fc7d370b3
nagaoka <kyoto.shima626@gmail.com>
2017-01-14 01:17:15 +0000

    20170114_challenge1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
@@ -18,10 +20,27 @@ class UsersController < ApplicationController
     end
   end
   
+  # ここから 課題1で追加
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'ユーザ情報を編集しました！'
+    else
+      render 'edit'
+    end
+  end
+  # ここまで
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :password, :location,
                                  :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102081036) do
+ActiveRecord::Schema.define(version: 20170114000824) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170102081036) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
